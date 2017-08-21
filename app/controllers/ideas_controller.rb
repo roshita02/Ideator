107b316bc3df1fd8cc3b4b80c9a28ebfbe4a3f3d
Roshita Shakya <roshi.shk@gmail.com>
2017-08-21 15:20:44 +0000

    Delete and revise links added

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -6,22 +6,27 @@ class IdeasController < ApplicationController
   def create
       @idea = Idea.create(idea_params)
       if @idea.valid?
-          #Implement later
+          #..
       else
-          #Implement later
+          #..
       end
-       redirect_to root_path
-  end
-  def edit
+    end
+ def edit
+  @idea = Idea.find(params[:id])
+end
         def update
-        @idea = Idea.find(params[:id])
+           @idea = Idea.find(params[:id])
         if @idea.update(idea_params)
              redirect_to root_path
          else
              redirect_to edit_idea_path(params[:id])
         end
-        end
     end
+  def destroy
+  @idea = Idea.find(params[:id])
+  @idea.destroy
+  redirect_to root_path
+end
   private
     def idea_params
         params.require(:idea).permit(:description, :author)
