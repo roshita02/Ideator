d47ea2f5a634d05822318a66d81bfab7db2561a2
Roshita Shakya <roshi.shk@gmail.com>
2017-08-22 10:45:36 +0000

    Static page controller set up

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'ideas#index'
     resources :ideas
+    
+    get 'about' => 'static_pages#about'
+    get 'random' => 'static_pages#random'
 end
