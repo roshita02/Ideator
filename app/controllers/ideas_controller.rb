dff0d7d1bf1e05de0e4175bc6b2316b77b99559f
Roshita Shakya <roshi.shk@gmail.com>
2017-08-23 08:52:27 +0000

    Flash messages added and errors resolved

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -3,28 +3,32 @@ class IdeasController < ApplicationController
     @ideas = Idea.all.paginate(:page => params[:page], :per_page => 4)
   end
 
-  def create
+def create
       @idea = Idea.create(idea_params)
       if @idea.valid?
-          #..
+          flash[:success] = "Your idea has been posted"
+          redirect_to root_path
       else
-          #..
+          flash[:alert] = "Looks like there's an error"
       end
-    end
- def edit
+end
+def edit
   @idea = Idea.find(params[:id])
 end
         def update
            @idea = Idea.find(params[:id])
         if @idea.update(idea_params)
+            flash[:success] = "Idea has been updated"
              redirect_to root_path
          else
+             flash[:alert] = "Looks like there's an error updating the idea"
              redirect_to edit_idea_path(params[:id])
         end
     end
   def destroy
   @idea = Idea.find(params[:id])
   @idea.destroy
+  flash[:success] = "The idea was successfully deleted"
   redirect_to root_path
 end
   private
